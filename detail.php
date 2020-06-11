2dfeacaadcd2ddf707ef8650aaf87f7b567bffef
welsonoktario <welsonoktario@yahoo.com>
2020-06-12 02:15:45 +0800

    detail kurang gambar

--- detail.php
+++ detail.php
@@ -13,23 +13,21 @@
 </head>
 
 <body>
-	<h1 class="text-2xl text-center font-bold">Detail Property</h1>
-	<div id="detail">Kategori Transaksi: Dijual</div>
-	<div id="jenis_property">Jenis Property: Rumah</div>
-	<div id="harga">Harga: Rp.500 Jt</div>
-	<div id="luas">
-		<div id="">Keterangan Bangunan</div>
-		<div id="kamar_tidur">Kamar tidur: 12</div>
-		<div id="kamar_mandi">Kamar mandi: 4</div>
-		<div id="jumlah_lantai">Jumlah lantai: 2</div>
-
-		<div id="">Keterangan Luas Tanah dan Bangunan</div>
-		<div id="luas_tanah">Luas Tanah: 123 m</div>
-		<div id="luas_bangunan">Luas Bangunan: 123 m</div>
+	<div class="container">
+		<h1 class="text-2xl text-center font-bold">Detail Property</h1>
+		<div id="kategori"></div>
+		<div id="jenis"></div>
+		<div id="harga"></div>
+		<div id="lt"></div>
+		<div id="lb"></div>
+		<div class="p-4">
+			<div class="text-xl font-bold">Detail Properti</div>
+			<div id="kt"></div>
+			<div id="km"></div>
+			<div id="lantai"></div>
+			<div id="furnished"></div>
+		</div>
 	</div>
-	<div id="long">long: 12312313</div>
-	<div id="lat">lat: -772637</div>
-	<div id="keterangan">lorep imsul gak au apa isi nya bangsat ku bosan wkwkwkwkw</div>
 	<script src="js/detail.js"></script>
 </body>
 
--- js/detail.js
+++ js/detail.js
@@ -1,8 +1,28 @@
 var urlParams = new URLSearchParams(window.location.search);
+var id = urlParams.get('id')
 
-$('#id').html(urlParams.get('id'));
-$('#harga').html(urlParams.get('harga'));
-// $('#luas_tanah').html(urlParams.get('id'));
-// $('#id').html(urlParams.get('id'));
-// $('#id').html(urlParams.get('id'));
-// $('#id').html(urlParams.get('id'));
+var kategori = $("#kategori");
+var jenis = $('#jenis');
+var harga = $("#harga");
+var lt = $("#lt");
+var lb = $("#lb");
+var kt = $("#kt");
+var km = $("#km");
+var lantai = $("#lantai");
+var furnished = $("#furnished");
+
+$.get(`http://localhost:8000/api/property/${id}`, function (data) {
+	var d = data[0];
+	var f = d.detail_property.furnished === 1 ? "Ya" : "Tidak";
+
+	console.log(d);
+	kategori.html(`Kategori: ${d.kategori.nama}`);
+	jenis.html(`Jenis: ${d.jenis_property.nama}`);
+	harga.html(`Harga: ${d.harga}`);
+	lt.html(`Luas Tanah: ${d.luas_tanah}`);
+	lb.html(`Luas Bangunan: ${d.luas_bangunan}`);
+	kt.html(`Jumlah Kamar Tidur: ${d.detail_property.jumlah_kamar_tidur}`);
+	km.html(`Jumlah Kamar Mandi: ${d.detail_property.jumlah_kamar_mandi}`);
+	lantai.html(`Lantai: ${d.detail_property.jumlah_lantai}`);
+	furnished.html(`Furnished: ${f}`)
+});
